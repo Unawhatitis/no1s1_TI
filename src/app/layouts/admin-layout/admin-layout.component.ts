616168cb4d904b9fe092470092c965f67c9abbbf
Unawhatitis <61868280+Unawhatitis@users.noreply.github.com>
2021-08-12 10:27:42 +0200

    updates

--- src/app/layouts/admin-layout/admin-layout.component.ts
+++ src/app/layouts/admin-layout/admin-layout.component.ts
@@ -33,24 +33,24 @@ export class AdminLayoutComponent implements OnInit {
               pointRadius: 0,
               pointHoverRadius: 0,
               borderWidth: 3,
-              data: [300, 310, 316, 322, 330, 326, 333, 345, 338, 354]
+              data: [0, 100, 185, 213, 250, 268, 333, 345, 338, 354]
             },
-            {
-              borderColor: "#f17e5d",
-              backgroundColor: "#f17e5d",
-              pointRadius: 0,
-              pointHoverRadius: 0,
-              borderWidth: 3,
-              data: [320, 340, 365, 360, 370, 385, 390, 384, 408, 420]
-            },
-            {
-              borderColor: "#fcc468",
-              backgroundColor: "#fcc468",
-              pointRadius: 0,
-              pointHoverRadius: 0,
-              borderWidth: 3,
-              data: [370, 394, 415, 409, 425, 445, 460, 450, 478, 484]
-            }
+            // {
+            //   borderColor: "#f17e5d",
+            //   backgroundColor: "#f17e5d",
+            //   pointRadius: 0,
+            //   pointHoverRadius: 0,
+            //   borderWidth: 3,
+            //   data: [320, 340, 365, 360, 370, 385, 390, 384, 408, 420]
+            // },
+            // {
+            //   borderColor: "#fcc468",
+            //   backgroundColor: "#fcc468",
+            //   pointRadius: 0,
+            //   pointHoverRadius: 0,
+            //   borderWidth: 3,
+            //   data: [370, 394, 415, 409, 425, 445, 460, 450, 478, 484]
+            // }
           ]
         },
         options: {
@@ -188,7 +188,7 @@ export class AdminLayoutComponent implements OnInit {
       };
 
       var speedData = {
-        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Ded"],
         datasets: [dataFirst, dataSecond]
       };
 
