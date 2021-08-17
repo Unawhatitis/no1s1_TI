f7c69cb5c9df2a47a7ff5f59464add933d1fc2f3
Unawhatitis <61868280+Unawhatitis@users.noreply.github.com>
2021-08-17 17:35:48 +0200

    update for deployment on testnet

--- migrations/2_no1s1_migration.js
+++ migrations/2_no1s1_migration.js
@@ -1,5 +1,9 @@
 const no1s1SMC = artifacts.require("no1s1data");
 
-module.exports = function (deployer) {
-  deployer.deploy(no1s1SMC);
+// module.exports = function (deployer) {
+//   deployer.deploy(no1s1SMC);
+// };
+
+module.exports = function (deployer, network, accounts) {
+  deployer.deploy(no1s1SMC,{from:accounts[0]});
 };
--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,12 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 // const infuraKey = "fj4jll3k.....";
 //
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+//const fs = require('fs');
+const mnemonic = "assist cabin humble engage sibling pipe bridge daughter project mass shift spider";
+//fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   /**
@@ -47,6 +48,14 @@ module.exports = {
       port: 8545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
      },
+     rinkeby: {
+      provider: function() {
+      return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/<INFURA_Access_Token>");
+      },
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000,
+      },
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
