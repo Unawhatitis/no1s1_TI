9a1516b8061701728af6f6f23a5a7282f89f25c5
Unawhatitis <61868280+Unawhatitis@users.noreply.github.com>
2021-09-15 21:43:58 +0200

    update front-end for smart contract

--- src/app/service/smc.service.ts
+++ src/app/service/smc.service.ts
@@ -3,7 +3,7 @@ const Web3 = require('web3');
 
 declare let require: any;
 declare let window: any;
-const tokenAbi = require('../../../build/contracts/no1s1data.json');
+const tokenAbi = require('../../../build/contracts/no1s1App.json');
 
 @Injectable({
   providedIn: 'root'
@@ -25,9 +25,9 @@ export class SMCService {
     this.defaultAcc="0x06Fa438ca3105d670C5F37Bd94113c28FE32a885";
   }
   
-  public registerNewUser(qusername,qcCode,qtime,quuid): Promise<any> {
+  public buyAccess(_duration,_username) : Promise<any> {
     return new Promise((resolve, reject) => {
-      this.no1s1.methods.initNewUser(qusername,qcCode,qtime,quuid).send({from:this.defaultAcc,gas:"6721975"}, function(err, data) {
+      this.no1s1.methods.buy(_duration,_username).send({from:this.defaultAcc,gas:"6721975"}, function(err, data) {
           if (err) {
             console.error(err);
             reject(err);
@@ -35,14 +35,19 @@ export class SMCService {
           console.log(data);
           resolve(data);
       });
-    //   this.no1s1.methods.initNewUser(qusername,qcCode,qtime,quuid).call(null, function(err1, data1) {
-    //     if (err1) {
-    //       console.error(err1);
-    //       reject(err1);
-    //     }
-    //     console.log(data1);
-    //     resolve(data1);
-    // });
+    }) as Promise<any>; 
+  }
+
+  public checkAccess(_key): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.no1s1.methods.checkAccess(_key).send({from:this.defaultAcc,gas:"6721975"}, function(err, data) {
+          if (err) {
+            console.error(err);
+            reject(err);
+          }
+          console.log(data);
+          resolve(data);
+      });
     }) as Promise<any>; 
   }
 
