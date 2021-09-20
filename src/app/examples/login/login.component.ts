ac02f4b903fe4606e5de071c15585944bceef056
Unawhatitis <61868280+Unawhatitis@users.noreply.github.com>
2021-09-20 10:05:50 +0200

    change login page

--- src/app/examples/login/login.component.ts
+++ src/app/examples/login/login.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators, ReactiveFormsModule} from '@angular/forms';
 import {TransferService} from '../../service/metamask.service';
 import {SMCService} from '../../service/smc.service';
 import {default as Web3} from 'web3';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -22,7 +23,14 @@ export class LoginComponent implements OnInit {
     Bstateofcharge:any;
     Aduration:any;
     Cost:any;
-
+    //***********duration form************//
+    //select duration 
+    buyDropdown = new FormControl();
+    durationSelected = false;
+    Duration = [5,10,20,40]
+    valueSubscription: Subscription;
+    selDuration : any;
+    //***********duration form************//
     // accountValidationMessages = {
     //   transferAddress: [
     //     { type: 'required', message: 'Transfer Address is required' },
@@ -51,6 +59,7 @@ export class LoginComponent implements OnInit {
         this.user = {address: '', transferAddress: '', balance: '', amount: ''};//, remarks: ''
         this.getAccountAndBalance();
         // this.createForms();
+        this.buyDropdown=this.fb.array(this.Duration);
 
         let that =this;
         this._smcService.returnLastLog().then(function(data){
@@ -59,6 +68,11 @@ export class LoginComponent implements OnInit {
         that.Cost=data[4];
       })
     }
+
+    //***********duration form************//
+
+
+
 
     logdata(event:Event):string{
       return (event.target as HTMLInputElement).value;
@@ -103,21 +117,29 @@ export class LoginComponent implements OnInit {
       }
 
     submitForm() {
-      // if (this.userForm.invalid) {
-      //   alert('transfer.components :: submitForm :: Form invalid');
-      //   return;
-      // } else {
-        // console.log('transfer.components :: submitForm :: this.userForm.value');
-        console.log(this.user.transferAddress);
-        console.log(this.user.amount);
-        this.transferService.transferEther(this.user).
-        then(function() {}).catch(function(error) {
-          console.log(error);
-        });
-        //no1s1 0x54B40cb7Cfa5485E8b4005D3Bb99CcE074DfF77a
-       //transfer 0xD18347324a633690E554E035DaC6b34461FB3373
-        //other contract 0xb98C6bE0927883Cf54dE02472F72997811e3661b
-      // }
+      this.selDuration = this.buyDropdown.get('');
+      console.log(this.buyDropdown.valueChanges.subscribe());
+      this.durationSelected=true;
+      //todo: add more transaction for escorn and stuff */
+
     }
 
+    //old submition form for transfer certain amount of ethereum to accounts//
+    // submitForm() {
+    //   if (this.userForm.invalid) {
+    //     alert('transfer.components :: submitForm :: Form invalid');
+    //     return;
+    //   } else {
+    //     console.log('transfer.components :: submitForm :: this.userForm.value');
+    //     console.log(this.user.transferAddress);
+    //     console.log(this.user.amount);
+    //     this.transferService.transferEther(this.user).
+    //     then(function() {}).catch(function(error) {
+    //       console.log(error);
+    //     });
+    //     no1s1 0x54B40cb7Cfa5485E8b4005D3Bb99CcE074DfF77a
+    //    transfer 0xD18347324a633690E554E035DaC6b34461FB3373
+    //     other contract 0xb98C6bE0927883Cf54dE02472F72997811e3661b
+    //   }
+    // }
 }
